872356a7f879ea3530a70522cf6c94b2eaff3381
vandyxd <vandanparakh@gmail.com>
2021-11-21 15:18:29 +0530

    first commit

---
+++ assets/javascript/jojo.js
@@ -0,0 +1,7 @@
+var audio = document.getElementById("audio");
+    app.audioElement = audio;
+    app.audioElement.volume = 0;
+
+var video = document.getElementById("background");
+    app.videoElement = video;
+    app.videoElement.volume = 0

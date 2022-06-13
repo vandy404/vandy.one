67b99831d69de959c74c01135b00b2db06f9e14b
vandy404 <vandanparakh@gmail.com>
2022-06-13 22:40:28 +0530

    yet another commit

--- assets/javascript/jojo.js
+++ assets/javascript/jojo.js
@@ -1,7 +1,7 @@
 var audio = document.getElementById("audio");
-    app.audioElement = audio;
-    app.audioElement.volume = 0;
+app.audioElement = audio;
+app.audioElement.volume = 0;
 
 var video = document.getElementById("background");
-    app.videoElement = video;
-    app.videoElement.volume = 0
+app.videoElement = video;
+app.videoElement.volume = 0;
